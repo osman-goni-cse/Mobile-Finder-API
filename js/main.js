ff64540fa6fb5203ec517df5805c054c2e2ab081
osman-goni-cse <osmancse32@gmail.com>
2022-03-02 00:27:42 +0600

    handle error

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const loadPhones = () => {
   const searchInput = document.getElementById('phone-text');
   const searchText = searchInput.value;
 
+  document.getElementById('error-message').style.display = 'none';
+
   fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
   .then(res => res.json())
   .then(data => displayPhones(data.data))
@@ -16,7 +18,10 @@ const loadPhones = () => {
 
 const displayPhones = (phones) => {
 
-  // console.log(phones)
+  console.log(phones)
+  if(phones.length == 0) {
+    document.getElementById('error-message').style.display = 'block';
+  }
   const length = Math.min(20, phones.length);
   // console.log(length);
   const showdetails = document.getElementById('show-details');
@@ -39,12 +44,11 @@ const displayPhones = (phones) => {
 
       div.innerHTML = `
       <div class="card h-100">
-        <img src="${phone.image}" class="card-img-top" alt="...">
+        <img src="${phone.image}" class="card-img-top mx-auto p-3" alt="..." style="width:250px">
         <div class="card-body">
           <h6 class="card-text"> <span class="fw-bold">Phone Name: </span> ${phone.phone_name} </h6>
           <h6 class="card-text"><span class="fw-bold">Brand Name: </span>${phone.brand}</h6>
-        </div>
-        <div class="card-footer">
+        
           <button onclick="loadDetails('${phone.slug}')" class="btn btn-primary w-100">Details</button>
         </div>
       </div>
@@ -64,25 +68,6 @@ const loadDetails = (phoneId) => {
   .then(data => showPhoneDetails(data.data))
 }
 
-// dynamic html
-
-// const dynamicTemplate = (phoneDetails) => {
-//   const tr = document.createElement('tr');
-//   const td = document.createElement('td');
-//   td.classList.add('table-data');
-//   td.classList.add('fw-bold');
-//   td.innerText = 'Sensors';
-
-//   for (const sensor of phoneDetails.mainFeatures.sensors) {
-//     const td = document.createElement('td')
-//     td.classList.add('table-data');
-//     td.innerText = sensor;
-//     tr.appendChild(td);
-//   }
-//   return tr;
-//   console.log(tr);
-// }
-
 // show Details of a phone
 
 const showPhoneDetails = (phoneDetails) => {
@@ -92,38 +77,10 @@ const showPhoneDetails = (phoneDetails) => {
   const showdetails = document.getElementById('show-details');
   showdetails.textContent = '';
 
-  // console.log(phoneDetails.mainFeatures)
-  // for(const [key, value] of Object.entries(phoneDetails.mainFeatures)) {
-    
-  //   if(!Array.isArray(value))
-  //     console.log(key, value)
-
-  // }style="width: 18rem;"
-
-  
-
-  /* This is */
-
-
-  
-  // const tr = document.createElement('tr');
-  // const td = document.createElement('td');
-  // td.classList.add('table-data');
-  // td.classList.add('fw-bold');
-  // td.innerText = 'Sensors';
-
-  // for (const sensor of phoneDetails.mainFeatures.sensors) {
-  //   const td = document.createElement('td')
-  //   td.classList.add('table-data');
-  //   td.innerText = sensor;
-  //   tr.appendChild(td);
-  // }
-
-  // console.log(tr);
 
   showdetails.innerHTML = `
-  <div style="width: 18rem;" class="card mx-auto">
-    <img src="${phoneDetails.image}" class="card-img-top" alt="..." style="height:>
+  <div style="width: 18rem; height: 500px; overflow-y:scroll;" class="card mx-auto">
+    <img src="${phoneDetails.image}" class="card-img-top p-3" alt="..." style="height:>
     <div class="card-body">
       
       <table class="w-100 mt-3">
@@ -167,19 +124,11 @@ const showPhoneDetails = (phoneDetails) => {
             <tr>
               <td class="fw-bold text-center"  > Sensors 
               </td>
+               
               <td>
-                ${phoneDetails.mainFeatures.sensors[0]?phoneDetails.mainFeatures.sensors[0]:''} <br>
-                ${phoneDetails.mainFeatures.sensors[1]?phoneDetails.mainFeatures.sensors[1]:''} <br> 
-                ${phoneDetails.mainFeatures.sensors[2]?phoneDetails.mainFeatures.sensors[2]:''} <br>
-                ${phoneDetails.mainFeatures.sensors[3]?phoneDetails.mainFeatures.sensors[3]:''} <br>
-                ${phoneDetails.mainFeatures.sensors[4]?phoneDetails.mainFeatures.sensors[4]:''} <br>
-                ${phoneDetails.mainFeatures.sensors[5]?phoneDetails.mainFeatures.sensors[5]:''} <br>
-                ${phoneDetails.mainFeatures.sensors[6]?phoneDetails.mainFeatures.sensors[6]:''} <br>
-                ${phoneDetails.mainFeatures.sensors[7]?phoneDetails.mainFeatures.sensors[7]:''} <br>
-                ${phoneDetails.mainFeatures.sensors[8]?phoneDetails.mainFeatures.sensors[8]:''}
-                
-                
-              </td> 
+                ${phoneDetails.mainFeatures.sensors.map(sensor => `
+                ${sensor}<br>`).join("\n")}
+              </td>
             </tr>
 
           </table>
@@ -226,3 +175,19 @@ const showPhoneDetails = (phoneDetails) => {
   </div>
   `
 }
+
+/*
+<td>
+                ${phoneDetails.mainFeatures.sensors[0]?phoneDetails.mainFeatures.sensors[0]:''} <br>
+                ${phoneDetails.mainFeatures.sensors[1]?phoneDetails.mainFeatures.sensors[1]:''} <br> 
+                ${phoneDetails.mainFeatures.sensors[2]?phoneDetails.mainFeatures.sensors[2]:''} <br>
+                ${phoneDetails.mainFeatures.sensors[3]?phoneDetails.mainFeatures.sensors[3]:''} <br>
+                ${phoneDetails.mainFeatures.sensors[4]?phoneDetails.mainFeatures.sensors[4]:''} <br>
+                ${phoneDetails.mainFeatures.sensors[5]?phoneDetails.mainFeatures.sensors[5]:''} <br>
+                ${phoneDetails.mainFeatures.sensors[6]?phoneDetails.mainFeatures.sensors[6]:''} <br>
+                ${phoneDetails.mainFeatures.sensors[7]?phoneDetails.mainFeatures.sensors[7]:''} <br>
+                ${phoneDetails.mainFeatures.sensors[8]?phoneDetails.mainFeatures.sensors[8]:''}
+                
+                
+              </td>
+*/
